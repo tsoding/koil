f14cb4125678d590d9797e7c20144667ee7db222
dj-viking <anders.swedishviking@gmail.com>
2024-06-23 14:30:37 -0400

    update ts ignore reason

--- watch.js
+++ watch.js
@@ -25,7 +25,7 @@ function cmd(program, args) {
     const p = spawn(program, args.flat(), spawnOptions); // NOTE: flattening the args array enables you to group related arguments for better self-documentation of the running command
     // @ts-ignore [stdout may be null?]
     p.stdout.on('data', (data) => process.stdout.write(data));
-    // @ts-ignore [stdout may be null?]
+    // @ts-ignore [stderr may be null?]
     p.stderr.on('data', (data) => process.stderr.write(data));
     p.on('close', (code) => {
         if (code !== 0) {
