0e3a9ae7496dfb92e61f9a94addf4ec06fd833e4
dj-viking <anders.swedishviking@gmail.com>
2024-06-23 14:22:43 -0400

    update for running npm commands on windows

--- watch.js
+++ watch.js
@@ -1,9 +1,31 @@
+// @ts-check
 const spawn = require('child_process').spawn;
 
+/**
+ * 
+ * @returns {boolean}
+ */
+function isWindows() {
+    return require("os").platform() === "win32";
+}
+
+/**
+ * @type {Parameters<typeof spawn>[2]}
+ */
+const spawnOptions = isWindows() ? { "shell": true } : {};
+
+/**
+ * 
+ * @param {string} program 
+ * @param {string[]} args 
+ * @returns {ReturnType<typeof spawn>}
+ */
 function cmd(program, args) {
-    console.log('CMD:', program, args);
-    const p = spawn(program, args.flat()); // NOTE: flattening the args array enables you to group related arguments for better self-documentation of the running command
+    console.log('CMD:', program, args.flat(), spawnOptions);
+    const p = spawn(program, args.flat(), spawnOptions); // NOTE: flattening the args array enables you to group related arguments for better self-documentation of the running command
+    // @ts-ignore [stdout may be null?]
     p.stdout.on('data', (data) => process.stdout.write(data));
+    // @ts-ignore [stdout may be null?]
     p.stderr.on('data', (data) => process.stderr.write(data));
     p.on('close', (code) => {
         if (code !== 0) {
