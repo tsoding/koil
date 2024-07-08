d87c1008ec914f932c282deccccd6ecdaabdc51a
rexim <reximkut@gmail.com>
2024-07-09 05:11:25 +0700

    Remove COS_OF_HALF_FOV
    It's not used anymore

--- game.ts
+++ game.ts
@@ -14,7 +14,6 @@ const EPS = 1e-6;
 const NEAR_CLIPPING_PLANE = 0.1;
 const FAR_CLIPPING_PLANE = 10.0;
 const FOV = Math.PI*0.5;
-const COS_OF_HALF_FOV = Math.cos(FOV*0.5);
 const PLAYER_SPEED = 2;
 const PLAYER_RADIUS = 0.5;
 
