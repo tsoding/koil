bef8c9bbc900924322547a8d9a509585165bdd1e
rexim <reximkut@gmail.com>
2025-04-02 22:10:09 +0700

    [common] Reimplement common::msg::batch::alloc in C

--- src/common.c
+++ src/common.c
@@ -1,5 +1,7 @@
 #include "common.h"
 
+void* allocate_temporary_buffer(size_t size);
+
 bool batch_message_verify_empty(MessageKind kind, Message *message) {
     // If message is empty it's byte_length must be equal to the size of BatchMessage exactly
     if (message->byte_length != sizeof(BatchMessage)) return false;
@@ -14,4 +16,12 @@ bool batch_message_verify(MessageKind kind, Message *message, size_t payload_siz
     BatchMessage* batch_message = (BatchMessage*)message;
     if ((MessageKind)batch_message->kind != kind) return false;
     return true;
+}
+
+BatchMessage *batch_message_alloc(MessageKind kind, size_t count, size_t payload_size) {
+    size_t byte_length = sizeof(BatchMessage) + payload_size*count;
+    BatchMessage *message = allocate_temporary_buffer(byte_length);
+    message->byte_length = byte_length;
+    message->kind = kind;
+    return message;
 }
